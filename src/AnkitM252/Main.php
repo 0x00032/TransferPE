133002e1489f772036ae0ed90045ef42c3643935
0x00032 <53447243+0x00032@users.noreply.github.com>
2021-11-10 15:10:41 -0500

    Updated :p

--- src/AnkitM252/Main.php
+++ src/AnkitM252/Main.php
@@ -7,12 +7,15 @@ use pocketmine\utils\TextFormat as TF;
 use pocketmine\command\{Command,CommandSender};
 
 class Main extends PluginBase implements Listener{
-	public function onEnable(){
-		$this->getServer()->registerEvents($this, $this);
+
+	public function onEnable(): void{
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->getLogger()->info(TF::GREEN . "TransferPE has been enabled!");
 	}
-	public function onCommand(CommandSender $sender, Command $command, $labels, array $args){
+
+	public function onCommand(CommandSender $sender, Command $command, $labels, array $args): bool{
 		$cmd = strtolower($command->getName());
+
 		if($cmd == "transferpe"){
 			if(isset($args[0]) && isset($args[1]) && isset($args[2])){
 				$player = $this->getServer()->getPlayer($args[0]);
@@ -22,20 +25,19 @@ class Main extends PluginBase implements Listener{
 					$sender->sendMessage(TF::RED . "That player is not online");
 				} else {
 					$player->sendMessage(TF::GREEN . "You are being transfered to ". $ip .":".$port);
-					$this->transferTo($player, $ip, $port);
+					$sender->sendMessage(TF::GREEN . "You transfered" . $player . "to: ". $ip .":".$port);
+					$player->transfer($ip, $port);
 				}
 			} else {
 				$sender->sendMessage(TF::RED . "Usage: /transferpe <player> <ip> <port>");
 			}
 		}
+
+		return true;
 	}
-	public function transferTo(Player $player, $ip, int $port){
-		$pk = new TransferPacket();
-		$pk->address = $ip;
-		$pk->port = $port;
-		Command::broadcastCommandMessage($player, "Transferred to " . $ip . ":" . $port);
-	}
+
 	public function onDisable(){
 		$this->getLogger()->info(TF::RED . "TransferPE has been disabled!");
 	}
+
 }
